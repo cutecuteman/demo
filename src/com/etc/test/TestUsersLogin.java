e8746c7bcc851e8ca1121e5a952b4a0066217c41
WenYi Qiu <1318022920@qq.com>
2018-07-06 10:39:32 +0800

    Revert "test"
    This reverts commit 5ceb359e6dc8246359e9a6a49e5f1842cd69f024.

--- src/com/etc/test/TestUsersLogin.java
+++ src/com/etc/test/TestUsersLogin.java
@@ -7,15 +7,19 @@ import com.etc.service.impl.UsersSerivceImpl;
 public class TestUsersLogin {
 
 	public static void main(String[] args) {
-		// TODO xxxxxxxxxx
+		// TODO Auto-generated method stub
 
 		UsersSerivce us = new UsersSerivceImpl();
 
+<<<<<<< HEAD
 		Users u = us.login("ssss", "123123123");
 		Users u1 = us.login("sasss", "12d3123123");
 		Users u2 = us.login("ds", "123123f123");
 		Users u3 = us.login("ssdss", "f");
 		
+=======
+		Users u = us.login("affff", "123456");
+>>>>>>> parent of 5ceb359... test
 
 		System.out.println(u);
 	}
